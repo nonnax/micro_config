bf381e37a47d2674f7d434da9ce8f2ea72727a30
nonnax <ironald@gmail.com>
2022-08-23 01:11:40 +0800

    update

--- plug/quoter/quoter.lua
+++ plug/quoter/quoter.lua
@@ -5,14 +5,14 @@ local config = import("micro/config")
 -- TODO: auto-indent when {} are used?
 
 local quotePairs = {
-  {"(",")"}, {"{","}"}, 
+  {"(",")"}, {"{","}"},
   {"[","]"}, {"<",">"},
-  {"\"", "\""}, {"'","'"}, 
-  {"`","`"}, {"_","_"}, 
-  {"-","-"},{"+","+"}, 
-  {"=","="}, {"|","|"}, 
-  {"%","%"}, {"/","/"}, 
-  {"*","*"}, {"~","~"}, 
+  {"\"", "\""}, {"'","'"},
+  {"`","`"}, {"_","_"},
+  {"-","-"},{"+","+"},
+  {"=","="}, {"|","|"},
+  {"%","%"}, {"/","/"},
+  {"*","*"}, {"~","~"},
   {":",":"}, {"?","?"}
 }
 
@@ -25,7 +25,7 @@ function preRune(bp, r)
 	if bp.Buf.Settings["quoter.enable"] == false  then
 		return true
 	end
-	if bp.Cursor:HasSelection() == false then		
+	if bp.Cursor:HasSelection() == false then
 		return true
 	end
 	for i = 1, #quotePairs do
@@ -36,7 +36,7 @@ function preRune(bp, r)
 	end
 end
 
-function quote(bp, open, close)	
+function quote(bp, open, close)
 	if not (-bp.Cursor.CurSelection[1]):GreaterThan(-bp.Cursor.CurSelection[2]) then -- is the first selection point later in the document than the second?
 		bp.Buf:Insert(-bp.Cursor.CurSelection[1], open)  -- right order
 		bp.Buf:Insert(-bp.Cursor.CurSelection[2], close) -- this happens almost always
