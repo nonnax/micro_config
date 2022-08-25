51b71b65e64491bd5e3daad1ad990c808f2162c5
nonnax <ironald@gmail.com>
2022-08-25 11:59:36 +0800

    cleanup

--- bin/micro_emmet.rb
+++ bin/micro_emmet.rb
@@ -1,15 +1,17 @@
 #!/usr/bin/env ruby
 # Id$ nonnax 2022-08-25 10:03:28
 
-current=nil
-@a=%w[a b c]
-
 def enclose(tag, tabstop:0, &block)
-  inside=block.call if block
-  tag=tag.gsub(/\#(\w+)/, ' id="\1"').gsub(/\.(\w+)/, ' class="\1"')
+  tag =
+  tag
+  .gsub(/\#(\w+)/, ' id="\1"')
+  .gsub(/\.(\w+)/, ' class="\1"')
+
   tagname=tag.scan(/\w+/).first
-  # "<%s>%s</%s>" % [tag, inside, tag]
+  inside=block.call if block
+
   tabs="  "*tabstop
+
   s="\n#{tabs}<%s>" % tag
   s<<"#{tabs*2}%s\n" % inside if block && !inside.strip.empty?
   s<<"\n#{tabs}</%s>" % tagname
@@ -18,27 +20,18 @@ end
 
 def process(a="", &block)
   arg=a.chomp.split(/>/)
-  # res=arg.dup.reverse.inject(str) do |acc, e|
-  acc=''
-  arg.dup.reverse.map.with_index do |e, i|
+  i=0
+  arg
+  .dup
+  .reverse
+  .inject("") do |acc, e|
     tag, n = e.split(/\*/)
     n ||= 1
-    tabstop=arg.size-i-1
-    acc=enclose(tag, tabstop:){acc}
+    tabstop=arg.size-(i=i.succ)
+    acc=enclose(tag, tabstop:){ acc }
     acc=acc*n.to_i
   end
-  .last
   .gsub(/\n{1,}/,"\n")
 end
 
-# pop c
-# process c
-# return "<c></c>"
-# pop b
-# process(b){doc}
-# return b
-
-# puts process(@a)
-a=gets
-puts res=process(a)
-puts res
+puts process(gets)
